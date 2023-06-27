30a914c38eb16ddb96c19c908b7b05236716c443
root <amuMabirimise>
2023-06-27 09:13:48 -0700

    handles.c

--- handles.c
+++ handles.c
@@ -10,11 +10,26 @@ int handles(void)
 	unsigned int octal = 63;
 	unsigned int lowercasehex = 255;
 	unsigned int uppercasehex = 255;
+	unsigned int inputDecimal;
+	unsigned int inputOctal;
+	unsigned int inputHex;
 
 	printf("Unsigned decimal value: %u\n", value);
 	printf("Octal value: %o\n", octal);
 	printf("Lowercase hexadecimal value: %x\n", lowercasehex);
 	printf("Uppercase hexadecimal value: %X\n", uppercasehex);
+
+	printf("Enter a decimal number: ");
+	scanf("%u", &inputDecimal);
+	printf("Decimal entered: %o\n", inputDecimal);
+
+	printf("Enter an octal number: ");
+	scanf("%o", &inputOctal);
+	printf("Octal entered: %o\n", inputOctal);
+
+	printf("Enter a hexadecimal number: ");
+	scanf("%x", &inputHex);
+	printf("Hexadecimal entered: %x\n", inputHex);
 
 	return (0);
 }
