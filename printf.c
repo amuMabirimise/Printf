ea257197fa540762eb4ccd7666316b3fa3f1d96a
root <amuMabirimise>
2023-06-27 09:30:44 -0700

    handles.h

--- printf.c
+++ printf.c
@@ -13,6 +13,13 @@ int _printf(const char *format, ...)
 {
         int printed_chars = 0;
         const char *ptr = format;
+	unsigned int decimal = 42;
+	unsigned int octal = 63;
+	unsigned int lowercasehex = 255;
+	unsigned int uppercasehex = 255;
+	unsigned int inputDecimal;
+	unsigned int inputOctal;
+	unsigned int inputHex;
 
         va_list args;
         va_start(args, format);
@@ -72,7 +79,24 @@ int _printf(const char *format, ...)
 
                 ptr++;
 
-        }
+	}
+
+	printf("Unsigned decimal: %u\n", decimal);
+	printf("Octal: %o\n", octal);
+	printf("Lowercase hexadecimal: %x\n", lowercasehex);
+	printf("Uppercase hexadecimal: %X\n", uppercasehex);
+
+	printf("Enter a decimal number: ");
+	scanf("%u", &inputDecimal);
+	printf("Decimal entered: %o\n", inputDecimal);
+
+	printf("Enter an octal number: ");
+	scanf("%o", &inputOctal);
+	printf("Octal entered: %o\n", inputOctal);
+
+	printf("Enter a hexadecimal number: ");
+	scanf("%x", &inputHex);
+	printf("Hexadecimal entered: %x\n", inputHex);
         va_end(args);
 
         return (printed_chars);
